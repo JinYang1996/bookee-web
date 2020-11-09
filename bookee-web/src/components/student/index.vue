3769cff912451b7af052d3d1ff907139ed758cb3
jy <243950661@qq.com>
2020-11-09 15:05:20 +0800

    slot完善

--- bookee-web/src/components/student/index.vue
+++ bookee-web/src/components/student/index.vue
@@ -1,14 +1,35 @@
 <template>
     <div class="slots">
-        slot的用法
+        <h2>slot的用法  可以用在自定义样式上</h2>
         <slotChild>
-            <template v-slot:slot-mid>
+            <!-- <template v-slot:slot-mid>
                 <div class="no-name">
                     我是嵌在子组件里不具有属性名的标签ffff
                 </div>
+            </template> -->
+            <template #default="{ maleSales,user }">
+                <div>{{user.firstName}}-{{user.lastName}}</div>
+                <div v-for="item in maleSales" :key="item.id">
+                    <input type="checkbox" v-model="item.isTrue">
+                    <span :style="colorObject">{{item.name}}</span>
+                </div>
             </template>
-            
         </slotChild>
+        <br>
+        <slotChild>
+            <template v-slot:slot-mid>
+                <div class="no-name">
+                    我是嵌在子组件里不具有属性名的标签6666
+                </div>
+            </template>
+            <template v-slot="{feMaleSales}">
+                <div v-for="item in feMaleSales" :key="item.id">
+                    <input type="checkbox" v-model="item.isTrue">
+                    <span :style="{color:'blue'}">{{item.name}}</span>
+                </div>
+            </template>
+        </slotChild>
+        <button @click="$router.back(-1)">返回</button>
     </div>
 </template>
 
@@ -21,7 +42,13 @@ export default {
     },
     data(){
         return {
-
+            user:{
+                firstName:'张',
+                lastName:'三'
+            },
+            colorObject:{
+                color:'red'
+            }
         }
     }
 }
--- bookee-web/src/components/student/slotChild.vue
+++ bookee-web/src/components/student/slotChild.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="slot-child">
-        <slot name="slot-mid"></slot>
-        我是slot组件fff
+        <slot name="slot-mid" :user="user">666666</slot>
+        <slot :maleSales="maleSales" :user="user" :feMaleSales="feMaleSales">
+            <div v-for="item in maleSales" :key="item.id">
+                <input type="checkbox" v-model="item.isTrue">
+                <span>{{item.name}}</span>
+            </div>
+        </slot>
+        我是slot组件
     </div>
 </template>
 
@@ -10,7 +16,44 @@ export default {
     name:'slotChild',
     data(){
         return {
-
+            user:{
+                firstName:'李',
+                lastName:'四'
+            },
+            maleSales:[
+                {
+                    id:'111',
+                    name:'jack',
+                    isTrue:true
+                },
+                {
+                    id:'222',
+                    name:'lucy',
+                    isTrue:true
+                },
+                {
+                    id:'333',
+                    name:'tom',
+                    isTrue:false
+                }
+            ],
+            feMaleSales:[
+                {
+                    id:'111',
+                    name:'jack',
+                    isTrue:null
+                },
+                {
+                    id:'222',
+                    name:'lucy',
+                    isTrue:null
+                },
+                {
+                    id:'333',
+                    name:'tom',
+                    isTrue:true
+                }
+            ]
         }
     }
 }
